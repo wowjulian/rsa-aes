afcc882c8a19a55ed85a0c63fdfae3bb4e0ad5b2
Julian <julian@linkcruit.com>
2025-03-08 11:50:47 -0800

    Add seeds for bbs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use num::{BigInt, BigUint, FromPrimitive, One, ToPrimitive, Zero};
 use num_bigint::{RandBigInt, ToBigInt, ToBigUint};
 use rand::thread_rng;
@@ -15,12 +17,10 @@ fn miller_rabin_test_q(n: BigUint) -> (u32, BigUint) {
 }
 fn miller_rabin_test(n: &BigUint, q: &BigUint, k: &u32) -> bool {
     let mut rng = rand::thread_rng();
-    let lbound = &BigUint::from_u8(2).unwrap();
+    let lbound = BigUint::one() + BigUint::one();
     let ubound = n - BigUint::one();
-    // println!("lbound: {} ubound: {}", lbound, ubound);
     let a: BigUint = rng.gen_biguint_range(&lbound, &ubound);
-    let a_pow_q_mod_n: BigUint = a.modpow(&q, n);
-    let is_inconclusive: bool = (a_pow_q_mod_n) == BigUint::one();
+    let is_inconclusive: bool = (a.modpow(&q, n)) == BigUint::one();
     if is_inconclusive {
         return true;
     }
@@ -46,7 +46,7 @@ fn miller_rabin_test(n: &BigUint, q: &BigUint, k: &u32) -> bool {
 
 fn is_prime(number: &BigUint) -> bool {
     let (k, q) = miller_rabin_test_q(number.clone());
-    for i in 0..128 {
+    for i in 0..56 {
         let inconclusive = miller_rabin_test(&number, &q, &k);
         if !inconclusive {
             return false;
@@ -55,17 +55,31 @@ fn is_prime(number: &BigUint) -> bool {
     return true;
 }
 
-fn generate_prime_number() {
+fn generate_prime_number() -> BigUint {
     let mut rng = rand::thread_rng();
     let mut odd_random_number = rng.gen_biguint(1024) | BigUint::one();
+    let mut count = 0;
     while !is_prime(&odd_random_number) {
+        count += 1;
         odd_random_number = rng.gen_biguint(1024) | BigUint::one();
     }
+    println!("Count is: {}", count);
     println!("Prime is: {}", odd_random_number);
+    return odd_random_number;
+}
+
+fn find_seed_prime_for_bbs() -> BigUint {
+    let mut p: BigUint = generate_prime_number();
+    while p.clone() % (4 as u8) != 3.to_biguint().unwrap() {
+        println!("% 4 = 3 not passed");
+        p = generate_prime_number();
+    }
+    return p;
 }
 
 fn main() {
-    let test = BigInt::from_u64(100).unwrap();
-    println!("test: {}", test + 1);
-    generate_prime_number();
+    let p: BigUint = BigUint::from_str("91122045179318965173533839131368998662772456836316619574148988450969399638066015732396427566243748625301463193721989348160150289310601464760678023543905884939640329370981639669486054016790003739067183295427192269871515101958634419380284904391739809184729932234982543491394799238889453600867187568552286325947").unwrap();
+    let q: BigUint = BigUint::from_str("22537017916243391647302294697783847565496398436656731187314486777281589031427811644162172298834653261847630683638084954095389795674883668957087575593936693030294860183039798443433626313894524118928724341320999385717299136781988248405248165926958106413623940227249989571125812352913420301394817441164820406503").unwrap();
+    println!("passing: {}", p);
+    println!("passing: {}", q);
 }
