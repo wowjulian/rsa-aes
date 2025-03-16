14170a9888aa70d5a0e307696a776454656d990f
Julian <julian@linkcruit.com>
2025-03-16 12:22:16 -0700

    Implement Task 5

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use aes_cbc::{dec_cbc, enc_cbc, get_iv};
 use bbs::blum_blum_shub;
 use libm::erfc;
 use num::{BigUint, Integer, One};
-use std::io::Write;
 use std::{fs::File, io::Read};
+use std::{io::Write, time::Instant};
 
 fn task_1() -> BigUint {
     // Task 1
@@ -119,9 +119,60 @@ fn task_4() {
     out_file.write_all(&decrypted).unwrap();
 }
 
+fn task_5() {
+    // Generate a AES symm key
+    let (_, _, aes_key) = blum_blum_shub(256);
+    println!("key: {}", aes_key);
+
+    // Generate q and p
+    let (q, p, _) = blum_blum_shub(2048);
+
+    // Calculate n and totient n
+    let n = q.clone() * p.clone();
+    let totient_n = (&q - BigUint::one()) * (&p - BigUint::one());
+
+    // Cacluate e
+    let mut e = BigUint::one() + BigUint::one();
+    loop {
+        if e >= totient_n {
+            panic!("could not calculate e");
+        }
+        if e.gcd(&totient_n) == BigUint::one() {
+            break;
+        }
+        e = e + BigUint::one();
+    }
+    // Calculate d
+    let d = e.modinv(&totient_n).unwrap();
+    // Encrpyt
+    let encrypted_aes_key = aes_key.modpow(&e, &n);
+
+    // Decrypt using modpow
+    let start = Instant::now();
+    let decrypted_aes_key = encrypted_aes_key.modpow(&d, &n);
+    let duration = start.elapsed();
+    println!("dec [{:?}]: {}", duration, decrypted_aes_key);
+    // Decrypt using CRT
+    let start = Instant::now();
+    let p_minus_one = p.clone() - BigUint::one();
+    let q_minus_one = q.clone() - BigUint::one();
+    let dp = &d % &p_minus_one;
+    let dq = &d % &q_minus_one;
+    let q_inv = q.modinv(&p).unwrap();
+    let m1 = encrypted_aes_key.modpow(&dp, &p);
+    let m2 = encrypted_aes_key.modpow(&dq, &q);
+    let h = (q_inv * (m1 - &m2)) % &p;
+    let m = m2 + h * q;
+    let duration = start.elapsed();
+    println!("dec [{:?}]: {}", duration, m);
+
+    // let dq = d.modpow(&BigUint::one(), &(q.clone() - BigUint::one()));
+}
+
 fn main() {
     // task_1();
-    task_2();
-    task_3();
+    // task_2();
+    // task_3();
     // task_4();
+    task_5();
 }
